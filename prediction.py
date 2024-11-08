121a90fce29932ad0c075d02f90962281fff433c
hasanmalik825-gh <gclab825@gmail.com>
2024-11-08 13:36:13 +0200

    added prediction.py

---
+++ prediction.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+# Step 1: Import Libraries and Load the Model
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.models import load_model
+
+# Load the IMDB dataset word index
+word_index = imdb.get_word_index()
+
+# Load the pre-trained model with ReLU activation
+model = load_model('simple_rnn_imdb.h5')
+
+# Step 2: Helper Functions
+
+# Function to preprocess user input
+def preprocess_text(text):
+    words = text.lower().split()
+    encoded_review = [word_index.get(word, 2) + 3 for word in words]
+    padded_review = sequence.pad_sequences([encoded_review], maxlen=500)
+    return padded_review
+
+### Prediction  function
+
+def predict_sentiment(review):
+    preprocessed_input=preprocess_text(review)
+
+    prediction=model.predict(preprocessed_input)
+
+    sentiment = 'Positive' if prediction[0][0] > 0.5 else 'Negative'
+
+    return sentiment, prediction[0][0]
+
+
+
+
+
+
+
